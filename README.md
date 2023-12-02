84c1a6863e70cc9746c779579bc1ba75bdff8b36
Andrew S. <asenger@gmail.com>
2023-12-02 15:36:38 -0500

    update for 1.0 release

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MLR RSS
-This code updates the MLR OOTC's Play by Play script currently hosted at https://github.com/MLROOTC/MLR-OOTC/tree/main/Python%20Scripts/Play%20by%20Play . This update includes the following:
-- multiple team feeds through the same file instance
+This code replaces the MLR OOTC's Play by Play script currently hosted at https://github.com/MLROOTC/MLR-OOTC/tree/main/Python%20Scripts/Play%20by%20Play . This update includes the following:
+- multiple MLR and MiLR team feeds through the same file instance
 - pinging of users when their at-bat is posted
 - ignoring of the bot's results posts
 
@@ -10,3 +10,30 @@ Future upgrades will include:
 - low-timer notifications to both the batter and GMs
 - pitcher notification
 - ability to be used for other leagues
+
+## Quick Setup Guide
+To get started, complete the following steps:
+
+### Save files
+Save all of the files into a directory on the computer you want to host the bot.
+
+### Edit config file
+Open `user_config.py` and update the settings as instructed. The bot is able to run unlimited teams through this user_config, as long as each team is configured.
+
+### Run the file
+Run `python main.py` (or `python3 main.py` on some builds). If all goes well, you should see no errors on the screen. If no errors appear, the bot is running and will check reddit every 15 seconds for new posts.
+
+If any errors appear while running, you'll need to investigate the reason why. Most of the time, it's due to improper information being stored in the user_config file. If you still cannot figure it out, you can get help through the following:
+* GitHub: Enter a new issue at https://github.com/yawetag/mlr_rss/issues and it will be investigated.
+* Let Stupido Einsteiny or Mark Schihne know through DMs on Discord and they will investigate.
+
+# Requirements
+The following Python libraries are required to run the MLR RSS bot. If you ever receive an error that the library isn't present, consult documentation on how to install libraries for your build:
+* datetime
+* dhooks
+* discord
+* pandas
+* praw
+* re
+* requests
+* time
--- user_config.py
+++ user_config.py
@@ -1,18 +1,27 @@
 import praw 
 from dhooks import Webhook
 
-SHEET = '1mwwPvV8FTXpjoUC4M11ttUydyLnBl2CKbwnufrtCqy0'
+# Note that for this file, if there are apostrophes present fill this out BETWEEN them.
 
+# Go to the current roster sheets. 
+# For SHEET, copy/paste this part of the URL: https://docs.google.com/spreadsheets/d/ -- COPY PASTE THIS -- /edit
+# For TAB, put the name of the tab that stores player names and MLR team, but replace any spaces with "%20".
+# As of Session 9.9, this should be written as "Player%20List".
+SHEET = '1mwwPvV8FTXpjoUC4M11ttUydyLnBl2CKbwnufrtCqy0'
 TAB = 'Player%20List'
 
 # praw = Python Reddit API Wrapper
 # go to https://www.reddit.com/prefs/apps
-# finish this description
+# hit "create an app..." at the bottom
+# fill in the form. hit "script" for the bubbles. fill in literally anything for the rest, it does not matter
+# you will see the name of your bot, then "personal use script", then your **client_id**. copy/paste this below.
+# below this (hit "edit" if it's hidden) you should see your **client_secret**. copy/paste this below.
+# name user_agent literally anything; it again doesn't matter.
 
 reddit = praw.Reddit(
     client_id     = '',
     client_secret = '',
-    user_agent    = ''
+    user_agent    = 'RSS'
 )
 
 # List of players who do *not* want to be pinged. This should be in the following format:
@@ -20,22 +29,23 @@ reddit = praw.Reddit(
 ping_exclude = []
 
 # List of RSS feeds for each league. Delete those you don't need and modify those you do, while maintaining the formatting.
+# To get the webhook: go to server settings, then integrations, then webhooks
+# create a new webhook. name it, pick what channel you want it in, and then hit the "copy webhook URL" button and paste it below.
 rss_feeds = [
     {
-        'search'  : '',
-        'abbrev'  : '',
-        'webhook' : Webhook(''),
-        'hexcode' : '',
-        'results' : True,
-        'ping'    : True
+        'search'  : 'Oakland Athletics',    # The program looks for new comments in posts with this in the title.
+        'abbrev'  : 'OAK',                  # Your team abbreviation.
+        'webhook' : Webhook('https://discord.com/api/webhooks/...'),   # The webhook URL you got
+        'hexcode' : '003831',               # The color of the vertical bar on the side (you choose)
+        'results' : True,                   # Does your team want to see the bot's result comments, or do you have the results webhook?
+        'ping'    : True,                   # Does your team want pings? You can leave this on and disable individual players above.
     }, 
-
     {
-        'search'  : '',
-        'abbrev'  : '',
-        'webhook' : Webhook(''),
-        'hexcode' : '',
+        'search'  : 'Golden Pride',
+        'abbrev'  : 'GOP',
+        'webhook' : Webhook('https://discord.com/api/webhooks/'),
+        'hexcode' : '7B2C88',
         'results' : False,
-        'ping'    : True
-    }
+        'ping'    : True,
+    },
 ]
